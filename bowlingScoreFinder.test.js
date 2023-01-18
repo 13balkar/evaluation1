ea7b959d9cb1d3cf8a8bae1da3edc43344c76379
Balkar_Singh <balkar81199@gmial.com>
2023-01-18 13:00:14 +0530

    feat: tests added

--- bowlingScoreFinder.test.js
+++ bowlingScoreFinder.test.js
@@ -2,21 +2,21 @@ const {getScore,findBestScore}=require('./bowlingScoreFinder');
 describe('Bowl score finder',()=>{
     describe('Calculate the score of each roll',()=>{
         it('Should throw error when input is not an array',()=>{
-            exports(()=>getScore("abc")).toThrow('Not an array');
+            expect(()=>getScore("abc")).toThrow('Not an array');
         })
         it('Should return error message when array is empty',()=>{
             expect(()=>getScore([])).toThrow('Empty error');
         });
         it('Should return error message when any element of array is not integer',()=>{
-            expect(()=>getScore([1,2,3,4,'a',5])).toThrow('All elements of array are not integers');
+            expect(()=>getScore([1,2,3,4,'a',5])).toThrow('Not all elements are numbers');
         });
         it('Should return total score when any element of array is as required',()=>{
-            expect(()=>getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+            expect(getScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
         });
     })
     describe('Calculate the best score of all rolls',()=>{
         it('Should throw error when input is not an array',()=>{
-            exports(()=>findBestScore("abc")).toThrow('Not an array');
+            expect(()=>findBestScore("abc")).toThrow('Not an array');
         })
         it('Should return error message when array is empty',()=>{
             expect(()=>findBestScore([])).toThrow('Empty error');
